1a56d07290ab028f3f1b159238fda86a8f45f02b
Alex <alexrasla@ucsb.edu>
2022-02-14 15:16:06 -0800

    plots and more data

--- stereo.py
+++ stereo.py
@@ -21,6 +21,7 @@ class Config:
     BASELINE = 160 #mm
     WINDOW_SIZE = 10 #experiment with different sizes, ask what is typical?
     OCCLUSION_COST = 100000
+    OCCLISION_FILLING = True
     DELTA = 1
     
 
@@ -47,8 +48,8 @@ def stereo_reconstruction(left_img_path, right_img_path, disparity_scale):
         left_stereo_img[scanline_idx, :left_disparity.shape[0]] = left_disparity #np.add(indexes - np.arange(indexes.shape[0]), disparity_scale)#Config.FOCAL_LENGTH * Config.BASELINE) / 
         right_stereo_img[scanline_idx, :right_disparity.shape[0]] = right_disparity
 
-        if scanline_idx%100 == 0:
-            print(scanline_idx)
+        if scanline_idx%300 == 0:
+            print('Scanline:', scanline_idx)
             # cv2.imshow('stereo', left_stereo_img)
             # cv2.waitKey(0)
    
@@ -98,19 +99,22 @@ def find_best_path(path_matrix, num_cols):
     left_disparity = np.zeros(num_cols)
     right_disparity = np.zeros(num_cols)
     
-    i, j, last_i, last_j = num_cols, num_cols, num_cols, num_cols
+    i, j, last_left_disp, last_right_disp = num_cols, num_cols, 0, 0
     
     while(i > 1 and j > 1):     
         if path_matrix[i,j] == 0:
-            left_disparity[i] = abs(i-j) # Disparity Image in Left Image coordinates
-            right_disparity[j] = abs(j-i) # Disparity Image in Right Image coordinates
-            i, last_i = i-1, i-1
-            j, last_j = j-1, j-1    
+            last_left_disp = abs(i-j)
+            last_right_disp = abs(j-i) 
+            
+            left_disparity[i] = last_left_disp # Disparity Image in Left Image coordinates
+            right_disparity[j] = last_right_disp # Disparity Image in Right Image coordinates
+            i = i-1
+            j = j-1   
         elif path_matrix[i, j] == 1:
-            left_disparity[i] = 0# left_disparity[last_i]
+            left_disparity[i] = last_left_disp
             i = i-1
         elif path_matrix[i,j] == 2:
-            right_disparity[j] = 0#right_disparity[last_j]
+            right_disparity[j] = last_right_disp
             j = j-1
             
     return left_disparity, right_disparity
@@ -161,34 +165,87 @@ def bmp_evalution(results, ground_truth, delta):
     
     return np.mean(values)
 
+def plot_values(data, save_dir, labels):
+    '''
+    Bar graph with evalution on 4 different images
+    Each plot of bar graph has 3 values, left dis, right dis, and accuracy... so 12 graphs
+    '''
+    
+    fig, axs = plt.subplots(2)
+    fig.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.5)
+    
+    x = np.arange(len(labels))
+
+    axs[0].set_title('Left and Right Stereo BMP Evaluations')
+    axs[0].set_ylabel('Bad Matched Pixel (BMP)')
+    axs[0].set_xlabel('Images Dataset')
+    axs[0].set_xticks(x + 0.125)
+    axs[0].set_xticklabels(labels)
+    axs[0].bar(x + 0.00, data[:, 0], width = 0.25, label='left') #left disparity
+    axs[0].bar(x + 0.25, data[:, 1], width = 0.25, label='right') #right disparity
+    axs[0].legend()
+    
+    axs[1].set_title('Time to Calculate Stereo Reconstruction')
+    axs[1].set_ylabel('Time (minutes)')
+    axs[1].set_xlabel('Images Dataset')
+    axs[1].bar(labels, data[:, 2], width = 0.25) #time
+
+    plt.savefig(os.path.join(save_dir, 'plots.png'))
+    
+
 if __name__ == "__main__":
     dir = args.dir
     left_path = args.left
     right_path = args.right
     disparity_scale = int(args.scale)
-    output_file = args.out
+    out_dir = args.out
+    #add eval argument
     
-    left_img_path = os.path.join(dir, left_path)
-    right_img_path = os.path.join(dir, right_path)
-    
-    start = time.time()
-    left_stereo_img, right_stereo_img = stereo_reconstruction(left_img_path, right_img_path, disparity_scale)
-    end = time.time()
-    
-    print("Elapsed time:", (end-start)/60.0)
-    
-    cv2.imwrite(os.path.join(dir, '1'+output_file), right_stereo_img)
-    cv2.imwrite(os.path.join(dir, '5'+output_file), left_stereo_img)
-    
-    # left_stereo_img = cv2.imread(os.path.join(dir, '1'+output_file))
-    # right_stereo_img = cv2.imread(os.path.join(dir, '5'+output_file))
-    
-    disp_right = cv2.imread(os.path.join(dir, 'disp1.png'))
-    disp_left = cv2.imread(os.path.join(dir, 'disp5.png'))
-    
-    left_bmp = bmp_evalution(left_stereo_img, disp_left, Config.DELTA)
-    right_bmp = bmp_evalution(right_stereo_img, disp_right, Config.DELTA)
-    
-    print('Left BMP:', left_bmp)
-    print('Right BMP:', right_bmp)
+    for dirpath, dirnames, filenames in os.walk(dir):
+        
+        print(dirpath, dirnames)
+        metrics = np.zeros((len(dirnames), 3))
+        
+        for sub_dir in range(len(dirnames)):  
+            current_dir = os.path.join(dir, dirnames[sub_dir])
+            
+            print('------------------------------')
+            print(current_dir) 
+            
+            left_img_path = os.path.join(current_dir, left_path)
+            right_img_path = os.path.join(current_dir, right_path)  
+            
+            start = time.time()
+            left_stereo_img, right_stereo_img = stereo_reconstruction(left_img_path, right_img_path, disparity_scale)
+            end = time.time()
+            
+            elapsed_time = (end-start)/60.0    
+            
+            cv2.imwrite(os.path.join(current_dir, 'res1.png'), right_stereo_img)
+            cv2.imwrite(os.path.join(current_dir, 'res5.png'), left_stereo_img)
+            
+            left_stereo_img = cv2.imread(os.path.join(current_dir, 'res1.png'))
+            right_stereo_img = cv2.imread(os.path.join(current_dir, 'res5.png'))
+            
+            disp_right = cv2.imread(os.path.join(current_dir, 'disp1.png'))
+            disp_left = cv2.imread(os.path.join(current_dir, 'disp5.png'))
+            
+            left_bmp = bmp_evalution(left_stereo_img, disp_left, Config.DELTA)
+            right_bmp = bmp_evalution(right_stereo_img, disp_left, Config.DELTA)
+                        
+            print('Left BMP:', left_bmp)
+            print('Right BMP:', right_bmp)
+            print("Elapsed time:", elapsed_time)
+            
+            metrics[sub_dir] = np.array([left_bmp, right_bmp, elapsed_time])
+
+        plot_values(metrics, dirpath, dirnames)
+        
+        break
+
     
